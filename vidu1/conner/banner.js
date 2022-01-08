e2e981ab9a93e1120d5c841c4e5d03101785f69a
taquangnghia <nghiatqph13235@fpt.edu.vn>
2022-01-08 15:23:29 +0700

    phan tacj

---
+++ vidu1/conner/banner.js
@@ -0,0 +1,8 @@
+const banner ={
+    print(){
+        return /* html*/ `
+        <img src="https://picsum.photos/1500/300" alt="" class="mt-2">;
+        `
+    },
+};
+export default banner;
---
+++ vidu1/new1.js
@@ -0,0 +1,53 @@
+const New1 ={
+    print (){
+        return /*html*/ `
+        <img src="https://picsum.photos/300/200"  class=" mx-auto mt-5" alt="">
+        <a  class="hover:bg-sky-700" href="#" ><h3 class="pl-11 font-bold mt-5 text-orange-500  ">1231sdasdasdasdasdasdasdsda23</h3></a>
+        <p class="pl-11 pr-11 mt-5">  Đối với tất cả các bạn sinh viên tham gia học tập tại Cao đẳng FPT Polytechnic: Các bạn sinh viên sẽ phải cần hoàn thành 04 Level Tiếng Anh (tương đương với trình độ Tiếng Anh TopNotch: 1 và 2) trong 4 học kỳ liên tục. Và là điều kiện tiên quyết đối với các bạn sinh viên để đủ điều kiện xét tốt nghiệp.
+
+          -        Các bạn sinh viên CÓ THỂ tham gia thi test Tiếng Anh đầu vào để phân loại trình độ Tiếng Anh của mình. Và được xếp lớp học phù hợp với trình độ.
+          
+          -        Sinh viên ĐẠT một trong các chứng chỉ quốc tế sau (các chứng chỉ có giá trị nội bộ, sẽ không được chấp nhận):
+          
+                  Chứng nhận TOEFL paper >= 450 hoặc TOEFL Ibt >=45.
+          
+                  Chứng nhận TOEIC >=550.
+          
+                  Chứng nhận IELTS >= 4.5</p>
+
+      </div>
+      <div class="border-2  mt-5">
+        <img src="https://picsum.photos/300/200"  class=" mx-auto mt-5" alt="">
+        <a href="#" class="hover:bg-sky-700" ><h3 class="pl-11 font-bold mt-5 text-orange-500">123123</h3></a>
+        <p class="pl-11 pr-11 mt-5">  Đối với tất cả các bạn sinh viên tham gia học tập tại Cao đẳng FPT Polytechnic: Các bạn sinh viên sẽ phải cần hoàn thành 04 Level Tiếng Anh (tương đương với trình độ Tiếng Anh TopNotch: 1 và 2) trong 4 học kỳ liên tục. Và là điều kiện tiên quyết đối với các bạn sinh viên để đủ điều kiện xét tốt nghiệp.
+
+          -        Các bạn sinh viên CÓ THỂ tham gia thi test Tiếng Anh đầu vào để phân loại trình độ Tiếng Anh của mình. Và được xếp lớp học phù hợp với trình độ.
+          
+          -        Sinh viên ĐẠT một trong các chứng chỉ quốc tế sau (các chứng chỉ có giá trị nội bộ, sẽ không được chấp nhận):
+          
+                  Chứng nhận TOEFL paper >= 450 hoặc TOEFL Ibt >=45.
+          
+                  Chứng nhận TOEIC >=550.
+          
+                  Chứng nhận IELTS >= 4.5</p>
+
+      </div>
+      <div class="border-2  mt-5">
+        <img src="https://picsum.photos/300/200"  class=" mx-auto mt-5" alt="">
+        <a href="#" ><h3 class="pl-11 font-bold mt-5 text-orange-500">123123</h3></a>
+        <p class="pl-11 pr-11 mt-5">  Đối với tất cả các bạn sinh viên tham gia học tập tại Cao đẳng FPT Polytechnic: Các bạn sinh viên sẽ phải cần hoàn thành 04 Level Tiếng Anh (tương đương với trình độ Tiếng Anh TopNotch: 1 và 2) trong 4 học kỳ liên tục. Và là điều kiện tiên quyết đối với các bạn sinh viên để đủ điều kiện xét tốt nghiệp.
+
+          -        Các bạn sinh viên CÓ THỂ tham gia thi test Tiếng Anh đầu vào để phân loại trình độ Tiếng Anh của mình. Và được xếp lớp học phù hợp với trình độ.
+          
+          -        Sinh viên ĐẠT một trong các chứng chỉ quốc tế sau (các chứng chỉ có giá trị nội bộ, sẽ không được chấp nhận):
+          
+                  Chứng nhận TOEFL paper >= 450 hoặc TOEFL Ibt >=45.
+          
+                  Chứng nhận TOEIC >=550.
+          
+                  Chứng nhận IELTS >= 4.5</p>
+        
+        `
+    },
+};
+export default New1;
